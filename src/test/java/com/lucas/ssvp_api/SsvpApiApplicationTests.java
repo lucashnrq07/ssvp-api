f85b5307f8fa85c8ceef56992c971cd8c4ae24ed
Lucas Henrique <lucashsilvaa8@gmail.com>
2026-04-03 11:03:01 -0300

    primeiro commit

---
+++ src/test/java/com/lucas/ssvp_api/SsvpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.ssvp_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsvpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
