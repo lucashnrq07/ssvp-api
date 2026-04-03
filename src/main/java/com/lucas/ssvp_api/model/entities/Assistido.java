23887bd583a3ed7072b3119d683ff35079598fb0
Lucas Henrique <lucashsilvaa8@gmail.com>
2026-04-03 12:01:50 -0300

    feat: cria entidade e migration de assistido

---
+++ src/main/java/com/lucas/ssvp_api/model/entities/Assistido.java
@@ -0,0 +1,36 @@
+package com.lucas.ssvp_api.model.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Table(name = "assistidos")
+public class Assistido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, unique = true, length = 15)
+    private String cpf;
+
+    @Column(nullable = false, length = 4)
+    private String pin;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal saldo = BigDecimal.ZERO;
+
+    @ManyToOne
+    @JoinColumn(name = "conferencia_id", nullable = false)
+    private Conferencia conferencia;
+}
