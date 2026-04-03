8e438868d146147935147edf17d9feb99e8e36b5
Lucas Henrique <lucashsilvaa8@gmail.com>
2026-04-03 14:04:58 -0300

    feat: adiciona dados iniciais de produtos

---
+++ src/main/resources/db/migration/V8__insert_produtos.sql
@@ -0,0 +1,7 @@
+INSERT INTO produtos (nome, preco) VALUES
+                                       ('Arroz', 10.00),
+                                       ('Feijão', 8.50),
+                                       ('Açúcar', 4.00),
+                                       ('Leite', 5.50),
+                                       ('Macarrão', 3.50),
+                                       ('Óleo', 7.00);
